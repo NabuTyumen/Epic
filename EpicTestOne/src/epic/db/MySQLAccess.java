6760e4c5290790de1eabf337366643d71f6ff9c9
BD <bruno.doussau@gmail.com>
2014-10-09 11:09:44 +0600

    maven + connection ok

--- EpicTestOne/src/epic/db/MySQLAccess.java
+++ EpicTestOne/src/epic/db/MySQLAccess.java
@@ -7,56 +7,64 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
+
 
 public class MySQLAccess {
-  private Connection con = null;
-  private Statement stmt = null;
-  private PreparedStatement ptmt = null;
-  private ResultSet rs = null;
+	private static String url = "jdbc:mysql://localhost/uruk?user=nabu&password=tyumen2014";
+	private static String login="gilgamesh";
+	private static String pw=".enk1du.";
+  private static Connection con = null;
+  private static Statement stmt = null;
+  private static PreparedStatement ptmt = null;
+  private static ResultSet rs = null;
+  
+  public static Connection getConnection(){
+	  try {
+	      // this will load the MySQL driver, each DB has its own driver
+	      Class.forName("com.mysql.jdbc.Driver");
+	      // setup the connection with the DB.
+	      con = DriverManager.getConnection(url);
 
-  public void readDataBase() throws Exception {
+	      return con;
+	      
+	    } catch (Exception e) {
+	      e.printStackTrace();
+	    } 
+	  return null;
+  }
+  
+  public static void updDataBase() throws Exception {
+	  getConnection();
+	   // preparedStatements can use variables and are more efficient
+      ptmt = con.prepareStatement("insert into uruk.god(name,title) values (?, ?)");
+      // "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
+      // parameters start with 1
+      ptmt.setString(1, "Enlil");
+      ptmt.setString(2, "Lord of the Storm");
+      ptmt.executeUpdate();
+  }
+
+  public static void readDataBase() throws Exception {
     try {
-      // this will load the MySQL driver, each DB has its own driver
-      Class.forName("com.mysql.jdbc.Driver");
-      // setup the connection with the DB.
-      con = DriverManager
-          .getConnection("jdbc:mysql://localhost/feedback?"
-              + "user=sqluser&password=sqluserpw");
-
+    	getConnection();
       // statements allow to issue SQL queries to the database
       stmt = con.createStatement();
       // resultSet gets the result of the SQL query
-      rs = stmt
-          .executeQuery("select * from FEEDBACK.COMMENTS");
+      rs = stmt.executeQuery("select * from uruk.god");
       writeResultSet(rs);
 
-      // preparedStatements can use variables and are more efficient
-      ptmt = con
-          .prepareStatement("insert into  FEEDBACK.COMMENTS values (default, ?, ?, ?, ? , ?, ?)");
-      // "myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
-      // parameters start with 1
-      ptmt.setString(1, "Test");
-      ptmt.setString(2, "TestEmail");
-      ptmt.setString(3, "TestWebpage");
-      ptmt.setDate(4, new java.sql.Date(2009, 12, 11));
-      ptmt.setString(5, "TestSummary");
-      ptmt.setString(6, "TestComment");
-      ptmt.executeUpdate();
 
-      ptmt = con
-          .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from FEEDBACK.COMMENTS");
+      ptmt = con.prepareStatement("SELECT name,title from uruk.god");
       rs = ptmt.executeQuery();
       writeResultSet(rs);
 
-      // remove again the insert comment
+      /* remove again the insert comment
       ptmt = con
       .prepareStatement("delete from FEEDBACK.COMMENTS where myuser= ? ; ");
       ptmt.setString(1, "Test");
-      ptmt.executeUpdate();
+      ptmt.executeUpdate();*/
       
-      rs = stmt
-      .executeQuery("select * from FEEDBACK.COMMENTS");
+      rs = stmt.executeQuery("select * from uruk.god");
       writeMetaData(rs);
       
     } catch (Exception e) {
@@ -67,7 +75,7 @@ public class MySQLAccess {
 
   }
 
-  private void writeMetaData(ResultSet resultSet) throws SQLException {
+  private static void writeMetaData(ResultSet resultSet) throws SQLException {
     // now get some metadata from the database
     System.out.println("The columns in the table are: ");
     System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
@@ -76,39 +84,60 @@ public class MySQLAccess {
     }
   }
 
-  private void writeResultSet(ResultSet resultSet) throws SQLException {
+  private static void writeResultSet(ResultSet resultSet) throws SQLException {
     // resultSet is initialised before the first data set
     while (resultSet.next()) {
-      // it is possible to get the columns via name
-      // also possible to get the columns via the column number
-      // which starts at 1
-      // e.g., resultSet.getSTring(2);
-      String user = resultSet.getString("myuser");
-      String website = resultSet.getString("webpage");
-      String summary = resultSet.getString("summary");
-      Date date = resultSet.getDate("datum");
-      String comment = resultSet.getString("comments");
-      System.out.println("User: " + user);
-      System.out.println("Website: " + website);
-      System.out.println("Summary: " + summary);
-      System.out.println("Date: " + date);
-      System.out.println("Comment: " + comment);
+    	int id = resultSet.getInt("idgod");
+      String god = resultSet.getString("name");
+      String title = resultSet.getString("title");
+      System.out.println("Id: " + id);
+      System.out.println("God: " + god);
+      System.out.println("Title: " + title);
     }
   }
 
   // you need to close all three to make sure
-  private void close() {
-    close((Closeable) rs);
-    close((Closeable) stmt);
-    close((Closeable) con);
+  private static void close() {
+	   try {
+		      if (rs != null) {
+		        rs.close();
+		      }
+		    } catch (Exception e) {
+		    // don't throw now as it might leave following closables in undefined state
+		    }
+	   
+	   try {
+		      if (stmt != null) {
+		        stmt.close();
+		      }
+		    } catch (Exception e) {
+		    // don't throw now as it might leave following closables in undefined state
+		    }
+	   try {
+		      if (ptmt != null) {
+		        ptmt.close();
+		      }
+		    } catch (Exception e) {
+		    // don't throw now as it might leave following closables in undefined state
+		    }
+	   try {
+		      if (con != null) {
+		        con.close();
+		      }
+		    } catch (Exception e) {
+		    // don't throw now as it might leave following closables in undefined state
+		    }
   }
-  private void close(Closeable c) {
-    try {
-      if (c != null) {
-        c.close();
-      }
-    } catch (Exception e) {
-    // don't throw now as it might leave following closables in undefined state
-    }
-  }
+
+  public static void main(String[] args) {
+	  try {
+		updDataBase();
+		readDataBase();
+	} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  
+
+	}
 } 
