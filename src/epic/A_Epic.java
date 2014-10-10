dde15b87fa9c2ad725023aa2319b4a9f3337233b
Olga <olga.chuenko@gmail.com>
2014-10-10 18:50:02 +0600

    kate_EpicAlgo

--- src/epic/A_Epic.java
+++ src/epic/A_Epic.java
@@ -10,14 +10,27 @@ package epic;
  */
 public class A_Epic {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
 		System.out.print(" ver. 01 - Gilgamesh says: \"I am Gilgamesh, mortal son of Lugalbanda and Ninsun, immortal daughter of Anu\".");
 		System.out.print(" ver. 02 - Enkidu says to Gilgamesh: \"I am Enkidu, mortal son of Ninurta, immortal \".");
 		System.out.print(" ver. 03 - Ishtar says to Gilgamesh: \"I am Ishtar, immortal daughter of Anu \".");
 		
-
-
+		for (int i=0; i<4; i++)
+			{
+			System.out.println(" ver. 0" + i + write (i));
+			}
+	}
+	
+	public static String write(int i) 
+	{
+		switch (i)
+		{
+			case 1:return "- Gilgamesh says: \"I am Gilgamesh, mortal son of Lugalbanda and Ninsun, immortal daughter of Anu\".";
+			case 2:return "- Enkidu says to Gilgamesh: \"I am Enkidu, mortal son of Ninurta, immortal \".";
+			case 3:return "- Ishtar says to Gilgamesh: \"I am Ishtar, immortal daughter of Anu \".";
+		}
+		return "";
 	}
 
 }
