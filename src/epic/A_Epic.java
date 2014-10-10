36757fa970680e4b938522f82fdd21dc4c2a8230
NabuTyumen <nabu.tyumen@gmail.com>
2014-10-10 19:59:48 +0600

    algo2, not work

--- src/epic/A_Epic.java
+++ src/epic/A_Epic.java
@@ -11,18 +11,15 @@ package epic;
 public class A_Epic {
 
 	public static void main(String[] args) {
+		String[] arNames = ("Gilgamesh","Enkidu","Ishtar");
+		String[] arType = ("mortal", "immortal");
+		String[] arGender = ("man", "woman");
+		String[] arParents = ("Lugalbanda", "Anu", "Ninurta", "Ninsun");
+		
 		
 		for (int i=1; i<4; i++) {
 			System.out.println(write_i(i));		
 		}
 
-	}
-	public static String write_i(int i){
-		switch(i){
-			case 1: return " ver. 01 - Gilgamesh says: \"I am Gilgamesh, mortal son of Lugalbanda and Ninsun, immortal daughter of Anu\".";
-			case 2: return " ver. 02 - Enkidu says to Gilgamesh: \"I am Enkidu, mortal son of Ninurta, immortal \".";
-			case 3: return " ver. 03 - Ishtar says to Gilgamesh: \"I am Ishtar, immortal daughter of Anu \".";
-		}
-		return "";
 	}
 }
