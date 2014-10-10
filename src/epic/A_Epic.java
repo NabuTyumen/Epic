a9480860c70b0fcceaf60cf9f423e88ea319839f
Kristina <kristinaer14@gmail.com>
2014-10-10 18:44:06 +0600

    kris_EpicAlgo1

--- src/epic/A_Epic.java
+++ src/epic/A_Epic.java
@@ -8,16 +8,23 @@ package epic;
  *to write our Epic
  *
  */
-public class A_Epic {
+public class epic {
 
-	public static void main(String[] args) {
-		
-		System.out.print(" ver. 01 - Gilgamesh says: \"I am Gilgamesh, mortal son of Lugalbanda and Ninsun, immortal daughter of Anu\".");
-		System.out.print(" ver. 02 - Enkidu says to Gilgamesh: \"I am Enkidu, mortal son of Ninurta, immortal \".");
-		System.out.print(" ver. 03 - Ishtar says to Gilgamesh: \"I am Ishtar, immortal daughter of Anu \".");
-		
-
-
+	public static String writer(int i){
+		switch(i){
+		case 1: return "Gilgamesh says: \"I am Gilgamesh, mortal son of Lugalbanda and Ninsun, immortal daughter of Anu\".";
+		case 2: return "Enkidu says to Gilgamesh: \"I am Enkidu, mortal son of Ninurta, immortal \".";
+		case 3: return "Ishtar says to Gilgamesh: \"I am Ishtar, immortal daughter of Anu \".";
+		}
+		return "";
 	}
+	
+	public static void main(String[] args) {
 
+		for (int i=1; i<4; i++)
+		{
+			System.out.println(" ver. 0" + i + " - " + writer(i));
+		}
+	}
+	
 }
