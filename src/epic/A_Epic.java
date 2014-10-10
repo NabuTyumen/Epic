600028720e4096968008d9d54a9da9a636d6f71b
Nikulin <Nikulinwin32@gmail.com>
2014-10-10 18:41:38 +0600

    Kirl_EpicAlgo1

--- src/epic/A_Epic.java
+++ src/epic/A_Epic.java
@@ -1,23 +1,19 @@
 package epic;
 
-/**
- * @author Bruno
- *
- *The simplest 
- *and less structured mean
- *to write our Epic
- *
- */
 public class A_Epic {
 
-	public static void main(String[] args) {
-		
-		System.out.print(" ver. 01 - Gilgamesh says: \"I am Gilgamesh, mortal son of Lugalbanda and Ninsun, immortal daughter of Anu\".");
-		System.out.print(" ver. 02 - Enkidu says to Gilgamesh: \"I am Enkidu, mortal son of Ninurta, immortal \".");
-		System.out.print(" ver. 03 - Ishtar says to Gilgamesh: \"I am Ishtar, immortal daughter of Anu \".");
-		
+    public static void main(String[] args) {
+    	for (int i=1; i<4; i++) {
+    		System.out.println(write(i));
+    	}
 
-
-	}
-
+                                           } 
+    public static String write(int i) {
+    	switch(i) {
+    	case 1: return " ver. 01 - Gilgamesh says: \"I am Gilgamesh, mortal son of Lugalbanda and Ninsun, immortal daughter of Anu\".";
+    	case 2: return " ver. 02 - Enkidu says to Gilgamesh: \"I am Enkidu, mortal son of Ninurta, immortal \".";
+    	case 3: return " ver. 03 - Ishtar says to Gilgamesh: \"I am Ishtar, immortal daughter of Anu \".";
+    	default:return "";
+    }
 }
+
