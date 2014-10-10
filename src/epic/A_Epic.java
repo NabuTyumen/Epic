001c9ab6167b604b1609a991cab6329b0c50caac
NabuTyumen <nabu.tyumen@gmail.com>
2014-10-10 18:50:59 +0600

    Andr epic_algo1

--- src/epic/A_Epic.java
+++ src/epic/A_Epic.java
@@ -12,12 +12,17 @@ public class A_Epic {
 
 	public static void main(String[] args) {
 		
-		System.out.print(" ver. 01 - Gilgamesh says: \"I am Gilgamesh, mortal son of Lugalbanda and Ninsun, immortal daughter of Anu\".");
-		System.out.print(" ver. 02 - Enkidu says to Gilgamesh: \"I am Enkidu, mortal son of Ninurta, immortal \".");
-		System.out.print(" ver. 03 - Ishtar says to Gilgamesh: \"I am Ishtar, immortal daughter of Anu \".");
-		
-
+		for (int i=1; i<4; i++) {
+			System.out.println(write_i(i));		
+		}
 
 	}
-
+	public static String write_i(int i){
+		switch(i){
+			case 1: return " ver. 01 - Gilgamesh says: \"I am Gilgamesh, mortal son of Lugalbanda and Ninsun, immortal daughter of Anu\".";
+			case 2: return " ver. 02 - Enkidu says to Gilgamesh: \"I am Enkidu, mortal son of Ninurta, immortal \".";
+			case 3: return " ver. 03 - Ishtar says to Gilgamesh: \"I am Ishtar, immortal daughter of Anu \".";
+		}
+		return "";
+	}
 }
